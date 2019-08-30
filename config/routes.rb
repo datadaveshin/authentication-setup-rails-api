ff359bc47d4af262a918c140aeb9efb72c154400
David Shin <davidshin444@gmail.com>
2019-08-29 17:10:41 -0700

    Add login and logout features to rails backend

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
+  get :logged_in, to: "sessions#logged_in"
+  delete :logout, to: "sessions#logout"
   root to: "static#home"
 end
