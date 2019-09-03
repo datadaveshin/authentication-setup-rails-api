8ea582c204db3af4a7cac208a37116af6382465b
David Shin <davidshin444@gmail.com>
2019-09-02 18:10:34 -0700

    Fix some bugs for registration in application, registrations controlers and cors initializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Removes the csrf token - as making an api 
+  # Removes the csrf token - as making an api
   skip_before_action :verify_authenticity_token
 end
